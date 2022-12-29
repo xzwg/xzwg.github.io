2f466bb34f42cddcad48663bee1af1d0ad889d8c
xzwg <109457203+xzwg@users.noreply.github.com>
2022-12-29 13:16:04 +0100

    Update randomvid.js

--- js/randomvid.js
+++ js/randomvid.js
@@ -7,9 +7,9 @@ let video = new Array();
 
 //theraceee
 video.push({
-	video_id: '8i4T7LF45R8',
+	video_id: 'P-qe28ZCHxY',
 	video_name: "devil",
-	id: '8i4T7LF45R8' + videoSettings + 'start=0',
+	id: 'P-qe28ZCHxY' + videoSettings + 'start=0',
 	note: 'race',
 	volume: 35,
 }); 
